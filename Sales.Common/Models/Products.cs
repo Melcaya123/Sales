faabfb6259a331fea65e2877fd276e3bd2e0cffc
Melcaya123 <Melcaya123@hotmail.com>
2018-08-20 14:16:47 -0500

    Create protect

--- Sales.Common/Models/Products.cs
+++ Sales.Common/Models/Products.cs
@@ -9,15 +9,45 @@
         public int ProductId { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Description { get; set; }
 
+        [DataType(DataType.MultilineText)]
+        public string Remarks { get; set; }
+
+        [Display (Name="Image")]
+        public String ImagePath { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
 
+        [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
 
+        [Display(Name = "Publish On")]
+        [DataType(DataType.Date)]
         public DateTime PublishOn { get; set; }
 
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.ImagePath))
+                {
+                    return null;
+                }
+                return $"https://salesapiservices.azurewebsites.net/{this.ImagePath.Substring(1)}";
+            }
+            
+        }
 
-        
+        public override string ToString()
+        {
+            return this.Description;
+        }
+
+
+
     }
 }
